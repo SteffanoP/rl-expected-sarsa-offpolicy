b43dbbc7b3be5c5d93497d3820964c77a7228ae3
Steffano Pereira <spereira@valcann.com.br>
2025-02-16 19:42:26 +0000

    feat: implement smoothing for Frozen Lake env

---
+++ util/plot.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def smooth(data, window):
+  data = np.array(data)
+  n = len(data)
+  y = np.zeros(n)
+  for i in range(n):
+    start = max(0, i-window+1)
+    y[i] = data[start:(i+1)].mean()
+  return y
